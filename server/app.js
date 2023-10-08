0ed59f68bf68ea87236d83c01b3d63fc629fc944
Maxim Makovskiy <makovskiyms@gmail.com>
2023-10-08 18:47:33 +0600

    Route all requests to index.html

--- server/app.js
+++ server/app.js
@@ -23,10 +23,11 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("dist"));
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
+
+app.use("/", express.static(__dirname + "/dist"));
+app.use("*", express.static(__dirname + "/dist"));
 
 app.use(errorHandler);
 
